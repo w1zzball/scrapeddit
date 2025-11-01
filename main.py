6163a33b5e4fc51f7f222818387385c1603a6ccb
w1zzball <aaronibbo@gmail.com>
2025-11-01 01:25:23 +0000

    retrieve comments from subreddit

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ PASSWORD = os.getenv("PASSWORD")
 CLIENT_ID = os.getenv("CLIENT_ID")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 SECRET_KEY = os.getenv("SECRET_KEY")
+USER_AGENT = os.getenv("USER_AGENT")
 
 client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID,SECRET_KEY)
 post_data = {"grant_type": "password", "username": USERNAME, "password": PASSWORD}
-headers = {"User-Agent": "bubblebotz/0.1 by bubblebotz"}
+headers = {"User-Agent": USER_AGENT}
 response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-print(response.json())
+token = response.json().get("access_token")
+headers = {"Authorization": f"bearer {token}", "User-Agent": USER_AGENT}
+
+
+def get_comments_from_subreddit(subreddit, limit):
+    url = f"https://oauth.reddit.com/r/{subreddit}/comments"
+    response = requests.get(url, headers=headers, params={"limit": limit})
+    print(response.json())
+
+get_comments_from_subreddit("python","5")
