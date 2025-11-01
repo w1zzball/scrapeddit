58d7b3579e4996fff354ce6b77608a7153cb2a43
w1zzball <aaronibbo@gmail.com>
2025-11-01 21:18:26 +0000

    update requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import requests
-import requests.auth
+import praw
 from dotenv import load_dotenv, find_dotenv
 import os
 load_dotenv(find_dotenv())
@@ -7,30 +6,13 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 CLIENT_ID = os.getenv("CLIENT_ID")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
-SECRET_KEY = os.getenv("SECRET_KEY")
+CLIENT_SECRET = os.getenv("SECRET_KEY")
 USER_AGENT = os.getenv("USER_AGENT")
 
-def get_token():
-    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID,SECRET_KEY)
-    post_data = {"grant_type": "password", "username": USERNAME, "password": PASSWORD}
-    headers = {"User-Agent": USER_AGENT}
-    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-    token = response.json().get("access_token")
-    return token
-
-token = get_token()
-headers = {"Authorization": f"bearer {token}", "User-Agent": USER_AGENT}
-
-
-def get_comments_from_subreddit(subreddit, params={"limit": 5, "sort":"new"}):
-    url = f"https://oauth.reddit.com/r/{subreddit}/comments"
-    response = requests.get(url, headers=headers, params=params)
-    return response.json()
-
-
-
-srjson = get_comments_from_subreddit("python")
-for child in srjson["data"]["children"]:
-    print(child["data"]["link_title"]) 
-    print(child["data"]["body"])
-    print("--"*20)
+reddit = praw.Reddit(
+    client_id= CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    password=PASSWORD,
+    user_agent="Comment Extraction (by u/USERNAME)",
+    username=USERNAME,
+)
