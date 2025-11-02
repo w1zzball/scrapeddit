7a3f1c157ddea67518f6f8abf2cd14097e96fc94
Aaron <155836753+w1zzball@users.noreply.github.com>
2025-11-02 16:47:59 +0000

    refactor code into bot class

--- main.py
+++ main.py
@@ -4,74 +4,79 @@ from asteval import Interpreter
 import os
 import io
 
-env_path = find_dotenv()
-if not env_path:
-    raise Exception(
-        "failed to import environment variables, does a .env exist in repo top level?"
-    )
-load_dotenv(env_path, override=True)
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
-CLIENT_ID = os.getenv("CLIENT_ID")
-# REDIRECT_URI = os.getenv("REDIRECT_URI")
-CLIENT_SECRET = os.getenv("SECRET_KEY")
-USER_AGENT = os.getenv("USER_AGENT")
 
-print(USER_AGENT)
+def load_auth_data_from_env() -> dict[str, str | None]:
+    env_path = find_dotenv()
+    if not env_path:
+        raise Exception(
+            "failed to import environment variables,"
+            " does a .env exist in repo top level?"
+        )
+    load_dotenv(env_path, override=True)
 
-# TODO refactor into bot class to scope reddit obj
-
-
-reddit = praw.Reddit(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    password=PASSWORD,
-    user_agent=USER_AGENT,
-    username=USERNAME,
-)
-
-print(reddit.read_only)
+    return {
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD"),
+        "client_id": os.getenv("CLIENT_ID"),
+        # REDIRECT_URI = os.getenv("REDIRECT_URI")
+        "client_secret": os.getenv("SECRET_KEY"),
+        "user_agent": os.getenv("USER_AGENT"),
+    }
 
 
-def post_on_subreddit(subreddit: str, title: str, text: str):
-    try:
-        reddit.subreddit(subreddit).submit(title, selftext=text)
-        print(f"Successfully posted to {subreddit}")
-    except Exception as e:
-        print(e.__str__())
+# TODO add logging
+class Bot:
+    def __init__(
+        self, *, username, password, client_id, client_secret, user_agent
+    ) -> None:
+        self.reddit = praw.Reddit(
+            username=username,
+            password=password,
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent=user_agent,
+        )
+
+    def post_on_subreddit(self, subreddit: str, title: str, text: str):
+        try:
+            self.reddit.subreddit(subreddit).submit(title, selftext=text)
+            print(f"Successfully posted to {subreddit}")
+        except Exception as e:
+            print(e.__str__())
+
+    def get_mentions(self, new: None | bool = None):
+        mentions = self.reddit.inbox.mentions(limit=25)
+        if new is not None:
+            return (m for m in mentions if m.new == new)
+        return mentions
+
+    def delete_read_mentions(self):
+        read_mentions = self.get_mentions(False)
+        for mention in read_mentions:
+            mention.delte()
+
+    def reply_to_unread_mentions(self, callback=lambda _: "test"):
+        unread = self.get_mentions(True)
+        for mention in unread:
+            # print(f"{mention.author}\n{mention.body}")
+            reply = mention.reply(callback(mention))
+            if reply is None:
+                raise Exception("could not reply")
+            mention.mark_read()
+
+    def safe_evaluate_comment(self, comment):
+        # TODO return eval errors to commenter
+        # TODO format input code to PEP8
+        str_buf = io.StringIO()
+        aeval = Interpreter(writer=str_buf)
+        aeval(comment.body)
+        return str_buf.getvalue()
 
 
-def get_mentions(new: None | bool = None):
-    mentions = reddit.inbox.mentions(limit=None)
-    if new is not None:
-        return (m for m in mentions if m.new == new)
-    return mentions
+auth_data = load_auth_data_from_env()
+bot = Bot(**auth_data)
 
-
-def delete_read_mentions():
-    read_mentions = get_mentions(False)
-    for mention in read_mentions:
-        mention.delte()
-
-
-def reply_to_unread_mentions(callback=lambda _: "test"):
-    unread = get_mentions(True)
-    for mention in unread:
-        # print(f"{mention.author}\n{mention.body}")
-        reply = mention.reply(callback(mention))
-        if reply is None:
-            raise Exception("could not reply")
-        mention.mark_read()
-
-
-def safe_evaluate_comment(comment):
-    # TODO return eval errors to commenter
-    # TODO format input code to PEP8
-    str_buf = io.StringIO()
-    aeval = Interpreter(writer=str_buf)
-    aeval(comment.body)
-    return str_buf.getvalue()
-
-
-# reply_to_unread_mentions(lambda c: f"you said '{c.body}'")
-# reply_to_unread_mentions(safe_evaluate_comment)
+# bot.reply_to_unread_mentions(lambda c: f"you said '{c.body}'")
+# bot.reply_to_unread_mentions(safe_evaluate_comment)
+for m in bot.reddit.inbox.all():
+    print(m)
