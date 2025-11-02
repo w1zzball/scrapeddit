2aab2048d36f87df1add41920026a3dd6ec93c4a
w1zzball <aaronibbo@gmail.com>
2025-11-02 12:54:19 +0000

    add env guard clause

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from dotenv import load_dotenv, find_dotenv
 from asteval import Interpreter
 import os
 import io
-load_dotenv(find_dotenv())
+
+env_path = find_dotenv()
+if(not env_path):
+    raise Exception("failed to import environment variables, does a .env exist in repo top level?")
+load_dotenv(env_path)
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 CLIENT_ID = os.getenv("CLIENT_ID")
 # REDIRECT_URI = os.getenv("REDIRECT_URI")
 CLIENT_SECRET = os.getenv("SECRET_KEY")
-# USER_AGENT = os.getenv("USER_AGENT")
+USER_AGENT = os.getenv("USER_AGENT")
 
+#TODO refactor into bot class to scope reddit obj
 reddit = praw.Reddit(
     client_id= CLIENT_ID,
     client_secret=CLIENT_SECRET,
     password=PASSWORD,
-    user_agent="Comment Extraction (by u/USERNAME)",
+    user_agent=USER_AGENT,
     username=USERNAME,
 )
 
@@ -47,10 +52,12 @@ def reply_to_unread_mentions(callback=lambda _ : "test"):
         mention.mark_read()
 
 def safe_evaluate_comment(comment):
+    #TODO return eval errors to commenter
+    #TODO format input code to PEP8
     str_buf = io.StringIO()
     aeval = Interpreter(writer=str_buf)
     aeval(comment.body)
     return str_buf.getvalue()
 
 # reply_to_unread_mentions(lambda c: f"you said '{c.body}'")
-reply_to_unread_mentions(safe_evaluate_comment)
+# reply_to_unread_mentions(safe_evaluate_comment)
