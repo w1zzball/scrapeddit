e8aa33678e75f269be6081922c3f625addd0e318
Aaron <155836753+w1zzball@users.noreply.github.com>
2025-11-04 20:58:34 +0000

    schema plan determined

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import praw
 from dotenv import load_dotenv, find_dotenv
-from asteval import Interpreter
 import os
-import io
 
 
 def load_auth_data_from_env() -> dict[str, str | None]:
@@ -37,41 +35,6 @@ class Bot:
             user_agent=user_agent,
         )
 
-    def post_on_subreddit(self, subreddit: str, title: str, text: str):
-        try:
-            self.reddit.subreddit(subreddit).submit(title, selftext=text)
-            print(f"Successfully posted to {subreddit}")
-        except Exception as e:
-            print(e.__str__())
-
-    def get_mentions(self, new: None | bool = None):
-        mentions = self.reddit.inbox.mentions(limit=25)
-        if new is not None:
-            return (m for m in mentions if m.new == new)
-        return mentions
-
-    def delete_read_mentions(self):
-        read_mentions = self.get_mentions(False)
-        for mention in read_mentions:
-            mention.delte()
-
-    def reply_to_unread_mentions(self, callback=lambda _: "test"):
-        unread = self.get_mentions(True)
-        for mention in unread:
-            # print(f"{mention.author}\n{mention.body}")
-            reply = mention.reply(callback(mention))
-            if reply is None:
-                raise Exception("could not reply")
-            mention.mark_read()
-
-    def safe_evaluate_comment(self, comment):
-        # TODO return eval errors to commenter
-        # TODO format input code to PEP8
-        str_buf = io.StringIO()
-        aeval = Interpreter(writer=str_buf)
-        aeval(comment.body)
-        return str_buf.getvalue()
-
     def get_top_level_comments_in_thread(
         self, post_id=None, post_url=None, limit: int | None = 0, threshold=0
     ):
@@ -91,7 +54,8 @@ class Bot:
         post_url=None,
         limit: int | None = 0,
         threshold=0,
-    ):
+    ) -> list[praw.models.Comment | praw.models.MoreComments]:
+        """Get all comments in a thread, returns a CommentForest object."""
         if post_id:
             comments = self.reddit.submission(post_id).comments
         elif post_url:
@@ -99,15 +63,24 @@ class Bot:
         else:
             raise Exception("provide either a post_id or post_url")
         comments.replace_more(limit=limit, threshold=threshold)
-        for comment in comments.list():
-            print(comment.body)
+        return comments.list()
+        # for comment in comments.list():
+        #     return comment.body
 
 
 auth_data = load_auth_data_from_env()
 bot = Bot(**auth_data)
 
-# bot.reply_to_unread_mentions(lambda c: f"you said '{c.body}'")
-# bot.reply_to_unread_mentions(safe_evaluate_comment)
-# for m in bot.reddit.inbox.all():
-#     print(m)
-bot.get_comments_in_thread("3g1jfi", limit=None)
+comments = bot.get_comments_in_thread("1oohc4a", limit=None)
+keys = [
+    "name",
+    "author",
+    "body",
+    "created_utc",
+    "edited",
+    "parent_id",
+    "submission",
+    "subreddit_name_prefixed",
+]
+for k in keys:
+    print(f"{k}: {getattr(comments[0], k, None)}")
---
+++ schema plan.md
@@ -0,0 +1,32 @@
+author: Redditor(name='ArcticWolfE') #author name
+body: 'I love that the willowy mod also somehow makes it weigh MORE than it would without it lol.'
+<!-- >body_html: '<div class="md"><p>I love that the willowy mod also somehow makes it weigh MORE than it would without it lol.</p>\n</div>' -->
+created: 1762286245.0
+created_utc: 1762286245.0 #unix POSIX epoch time
+downs: 0
+edited: False
+id: 'nn428xe' #comment id
+name: 't1_nn428xe' #unique (PK)
+parent_id: 't3_1oohc4a' #post this post is replying to
+<!-- >permalink: '/r/cavesofqud/comments/1oohc4a/spheres_of_negative_weight_can_spawn_with_the/nn428xe/' -->
+submission: Submission(id='1oohc4a') #OP post id
+subreddit_name_prefixed: 'r/cavesofqud'
+ups: 6 #upvotes
+
+
+----
+name: 't1_nn428xe' #unique (PK)
+
+author: Redditor(name='ArcticWolfE') #author name
+
+body: 'I love that the willowy mod also somehow makes it weigh MORE than it would without it lol.'
+
+created_utc: 1762286245.0 #unix POSIX epoch time
+<!-- downs: 0 -->
+edited: False
+
+parent_id: 't3_1oohc4a' #post this post is replying to
+
+submission: Submission(id='1oohc4a') #OP post id
+
+subreddit_name_prefixed: 'r/cavesofqud'
