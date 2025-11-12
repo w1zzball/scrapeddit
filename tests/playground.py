8851f920df5e7e05c8b975b464ce50f22408675b
Aaron <155836753+w1zzball@users.noreply.github.com>
2025-11-12 18:55:11 +0000

    rename io_utils to prompt

--- tests/playground.py
+++ tests/playground.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from scrapeddit.utils.io_utils import prompt_loop
+from scrapeddit.utils.prompt import prompt_loop
 from scrapeddit.utils.reddit_utils import (
     get_submission,
     format_submission,
@@ -19,14 +19,14 @@ from scrapeddit.utils.scraping_utils import (
 
 
 def main():
-    # prompt_loop()
+    prompt_loop()
     # comments = get_redditors_comments("bubblebotz", 5)
     # for comment in comments:
     #     formatted = format_comment(comment)
     #     print(formatted)
     # redditors = db_get_redditors_from_subreddit("cats", 100)
     # print(redditors)
-    scrape_redditor("bubblebotz", sort="top", limit=10)
+    # scrape_redditor("bubblebotz", sort="top", limit=10)
 
 
 if __name__ == "__main__":
