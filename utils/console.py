b20edb2b5a7b21cd3c46764553004a1ec1310869
Aaron <155836753+w1zzball@users.noreply.github.com>
2025-11-13 04:16:41 +0000

    add getter/setter to console

--- utils/console.py
+++ utils/console.py
@@ -2,3 +2,14 @@ from rich.console import Console
 
 # shared console instance
 console = Console()
+
+
+def set_console(new_console: Console) -> None:
+    """Replace the shared console instance."""
+    global console
+    console = new_console
+
+
+def get_console() -> Console:
+    """Return the shared console instance."""
+    return console
