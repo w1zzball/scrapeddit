1a4fcac551163bb92e036369d5fab01366ec2c49
Aaron <155836753+w1zzball@users.noreply.github.com>
2025-12-03 10:44:43 +0000

    fix error rasing on no value w/ get submission

--- utils/connection_utils.py
+++ utils/connection_utils.py
@@ -24,6 +24,7 @@ def reddit_session() -> Generator[praw.Reddit, None, None]:
         yield reddit
     except Exception as e:
         logger.error("Reddit session error: %s", e)
+        raise (e)
     finally:
         del reddit
 
